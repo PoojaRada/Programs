71393fed13623a9faff738d8da364d2ee717a630
Pooja Rada <bhavanarada@gmail.com>
2020-12-21 11:03:32 -0800

    Create String_as_Specified.py

---
+++ Python/String_as_Specified.py
@@ -0,0 +1,16 @@
+""" Write a Python program to print the following string in a specific format.
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are 
+  
+  Practice Source: https://www.w3resource.com/python-exercises/python-basic-exercises.php"""
+  
+print("Twinkle, twinkle, little star,")
+print("\n\t How I wonder what you are!")
+print("\n\t\tUp above the world so high,")
+print("\n\t\tLike a diamond in the sky")
+print("\n Twinkle, twinkle, little star,")
+print("\n\t How I wonder what you are!")
